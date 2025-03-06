1ed33ef28dd8ad17d273c335f7d4f0a3696a5889
ka be <kabecoder@gmail.com>
2025-03-06 17:13:21 +0800

    (feat): remove unused imports

--- app/src/main/java/com/kabeCoder/pokemonapp/pokemon/domain/PokemonDetail.kt
+++ app/src/main/java/com/kabeCoder/pokemonapp/pokemon/domain/PokemonDetail.kt
@@ -1,5 +1,7 @@
 package com.kabeCoder.pokemonapp.pokemon.domain
 
+import com.kabeCoder.pokemonapp.BuildConfig
+
 data class PokemonDetail(
     val name: String,
     val order: Int,
@@ -22,11 +24,10 @@ data class TypeInfo(
     val url: String
 )
 
-
 fun PokemonDetail.toPokemon(): Pokemon {
     return Pokemon(
         name = this.name,
-        url = "https://pokeapi.co/api/v2/pokemon/${this.order}/",
+        url = "${BuildConfig.POKEMON_API_BASE_URL}/pokemon/${this.order}/",
         order = this.order,
         types = this.types.map { it.type.name },
         weight = this.weight,
